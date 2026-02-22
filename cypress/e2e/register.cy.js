a67ff908d895493d5603664f149a8930058d6502
João Willian Belarmino Spejo <joaowillian.spejo@outlook.com>
2026-02-22 14:03:53 -0300

    changing to page object model

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,3 +1,5 @@
+import createPage from '../support/pages/create';
+
 const data = require('../fixtures/orphanages.json');
 
 describe('Cadastro de orfanatos', () => {
@@ -8,32 +10,10 @@ describe('Cadastro de orfanatos', () => {
 
     cy.deleteOrphanage(orphanage);
 
-    cy.goTo('/orphanages/create', -24.0191918, -46.4296952);
-
-    cy.get('.create-orphanage-form legend')
-      .should('be.visible')
-      .should('have.text', 'Cadastro');
-
+    createPage.go();
     cy.setMapCoordinates(orphanage.latitude, orphanage.longitude);
-
-    cy.get('#name').type(orphanage.name);
-    cy.get('#description').type(orphanage.description);
-    cy.get('#opening_hours').type(orphanage.opening_hours);
-
-    if (orphanage.open_on_weekends == true) {
-      cy.contains('button', 'Sim').click();
-    } else {
-      cy.contains('button', 'Não').click();
-    }
-
-    const images = orphanage.images.map((image) => `cypress/fixtures/${image}`);
-
-    cy.get('input[type="file"]').selectFile(images, {
-      force: true,
-      action: 'select', // Garante que ele trate as strings como caminhos de arquivos
-    });
-
-    cy.get('.create-orphanage-form .save-button').click();
+    createPage.form(orphanage);
+    createPage.submit();
 
     cy.get('.swal2-title').should('be.visible').should('have.text', 'Uhull!');
 
@@ -51,32 +31,13 @@ describe('Cadastro de orfanatos', () => {
 
     cy.postOrphanage(orphanage);
 
-    cy.goTo('/orphanages/create', -24.0191918, -46.4296952);
-
-    cy.get('.create-orphanage-form legend')
-      .should('be.visible')
-      .should('have.text', 'Cadastro');
+    createPage.go();
 
     cy.setMapCoordinates(orphanage.latitude, orphanage.longitude);
 
-    cy.get('#name').type(orphanage.name);
-    cy.get('#description').type(orphanage.description);
-    cy.get('#opening_hours').type(orphanage.opening_hours);
+    createPage.form(orphanage);
 
-    if (orphanage.open_on_weekends == true) {
-      cy.contains('button', 'Sim').click();
-    } else {
-      cy.contains('button', 'Não').click();
-    }
-
-    const images = orphanage.images.map((image) => `cypress/fixtures/${image}`);
-
-    cy.get('input[type="file"]').selectFile(images, {
-      force: true,
-      action: 'select', // Garante que ele trate as strings como caminhos de arquivos
-    });
-
-    cy.get('.create-orphanage-form .save-button').click();
+    createPage.submit();
 
     cy.get('.swal2-title').should('be.visible').should('have.text', 'Oops!');
 
