6b4870dfdf9697214d753755792ec8ace1ecf84e
João Willian Belarmino Spejo <joaowillian.spejo@outlook.com>
2026-02-24 13:02:19 -0300

    add required fields orphanage tests

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -31,29 +31,81 @@ describe('Cadastro de orfanatos', () => {
     cy.modalHaveText(`Já existe um cadastro com o nome: ${orphanage.name}`);
   });
 
-  it('não deve cadastrar um orfanato sem preencher os campos obrigatórios', () => {
-    const randomOrphanage = Math.floor(Math.random() * data.orphanages.length);
+  context('campos obrigatórios', () => {
+    it('não deve cadastrar um orfanato sem preencher o campo "Nome"', () => {
+      const randomOrphanage = Math.floor(
+        Math.random() * data.orphanages.length,
+      );
 
-    let orphanage = data.orphanages[randomOrphanage];
+      let orphanage = data.orphanages[randomOrphanage];
 
-    delete orphanage.latitude;
-    delete orphanage.longitude;
-    delete orphanage.name;
-    delete orphanage.description;
-    delete orphanage.images;
-    delete orphanage.opening_hours;
+      delete orphanage.name;
 
-    cy.goToCreate();
+      cy.goToCreate();
 
-    cy.createOrphanage(orphanage);
+      cy.createOrphanage(orphanage);
 
-    cy.alertHaveText(
-      '.map .leaflet-container',
-      'Informe a localizaçao no mapa',
-    );
-    cy.alertHaveText('#name', 'Campo obrigatório');
-    cy.alertHaveText('#description', 'Campo obrigatório');
-    cy.alertHaveText('input[type="file"]', 'Envie pelo menos uma foto');
-    cy.alertHaveText('#opening_hours', 'Campo obrigatório');
+      cy.alertHaveText('#name', 'Campo obrigatório');
+    });
+
+    it('não deve cadastrar um orfanato sem preencher o campo "Sobre"', () => {
+      const randomOrphanage = Math.floor(
+        Math.random() * data.orphanages.length,
+      );
+
+      let orphanage = data.orphanages[randomOrphanage];
+
+      delete orphanage.description;
+
+      cy.goToCreate();
+
+      cy.createOrphanage(orphanage);
+
+      cy.alertHaveText('#description', 'Campo obrigatório');
+    });
+
+    it('não deve cadastrar um orfanato sem enviar ao menos uma foto', () => {
+      const randomOrphanage = Math.floor(
+        Math.random() * data.orphanages.length,
+      );
+
+      let orphanage = data.orphanages[randomOrphanage];
+
+      delete orphanage.images;
+
+      cy.goToCreate();
+
+      cy.createOrphanage(orphanage);
+
+      cy.alertHaveText('input[type="file"]', 'Envie pelo menos uma foto');
+    });
+
+    it('não deve cadastrar um orfanato sem preencher os campos obrigatórios', () => {
+      const randomOrphanage = Math.floor(
+        Math.random() * data.orphanages.length,
+      );
+
+      let orphanage = data.orphanages[randomOrphanage];
+
+      delete orphanage.latitude;
+      delete orphanage.longitude;
+      delete orphanage.name;
+      delete orphanage.description;
+      delete orphanage.images;
+      delete orphanage.opening_hours;
+
+      cy.goToCreate();
+
+      cy.createOrphanage(orphanage);
+
+      cy.alertHaveText(
+        '.map .leaflet-container',
+        'Informe a localizaçao no mapa',
+      );
+      cy.alertHaveText('#name', 'Campo obrigatório');
+      cy.alertHaveText('#description', 'Campo obrigatório');
+      cy.alertHaveText('input[type="file"]', 'Envie pelo menos uma foto');
+      cy.alertHaveText('#opening_hours', 'Campo obrigatório');
+    });
   });
 });
